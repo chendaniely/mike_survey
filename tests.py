ee41fe8824375195b3646f922169408f83752be6
Daniel Chen <Maveri2k@yahoo.com>
2014-10-12 01:36:22 -0400

    test bp_and_pulse, parse_condition_int

--- tests.py
+++ tests.py
@@ -16,3 +16,16 @@ def test_read_condition_file():
     condition_responses = goals.read_condition_file_to_list(condition)
     print condition_responses
     assert len(condition_responses) == 11
+
+
+def test_test_bp_and_pulse():
+    d, s, p = goals.take_bp_and_pulse()
+    assert d == 120
+    assert s == 80
+    assert p == 60
+
+
+def test_parse_condition_file_to_int():
+    file_dir = "./messageConditions/condition_4_onetime_fair.txt"
+    condition_num = goals.parse_condition_file_to_int(file_dir)
+    assert condition_num == '4'
