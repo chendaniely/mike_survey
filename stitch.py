171527d9ec774243900340a2abc6fbb588d183cc
Daniel Chen <Maveri2k@yahoo.com>
2014-10-19 23:50:13 -0400

    tab to spaces

--- stitch.py
+++ stitch.py
@@ -3,7 +3,7 @@ import sys
 fp = open(sys.argv[2], 'w')
 
 for n, file in enumerate(open(sys.argv[1])):
-	for m, line in enumerate(open(file.rstrip())):
-        	dat = line.rstrip().split(',')
-	        dat.insert(0, file.rstrip().split('.txt')[0])
-        	fp.write('%s\n' % ','.join(dat))
+    for m, line in enumerate(open(file.rstrip())):
+            dat = line.rstrip().split(',')
+            dat.insert(0, file.rstrip().split('.txt')[0])
+            fp.write('%s\n' % ','.join(dat))
