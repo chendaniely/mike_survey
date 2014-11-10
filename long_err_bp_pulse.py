fixed nones in average, pivot long to wide

long_err_bp_pulse.py
@@ -1,35 +1,41 @@
 
 # coding: utf-8
 
-# In[9]:
+# In[47]:
 
 import pandas as pd
 import numpy as np
 import glob
 
 
-# In[10]:
+# In[48]:
+
+pwd
+
+
+# In[49]:
 
 response_files = glob.glob("*.txt")
 response_files
 
 
-# In[11]:
+# In[50]:
 
 def data_set_column_names():
     colnames = ['time', 'orgi_trial', 'adv', 'mksh', 'revg', 'price_d', 'price_r', 'estm_orig', 'time_r', 
                 'participant_response', 'condition_number', 
                 'bp_s', 'bp_d', 'pulse']
+    assert isinstance(colnames, list)
     return colnames
 
 
-# In[12]:
+# In[51]:
 
 def read_into_dataframe(response_file):
     return pd.read_csv(response_file, names = data_set_column_names())
 
 
-# In[13]:
+# In[52]:
 
 def generate_sections():
     my_list = []
@@ -40,123 +46,88 @@ def generate_sections():
     return pd.DataFrame(my_list, columns=['section_number'])
 
 
-# In[15]:
+# In[60]:
 
-df_all = pd.DataFrame()
-for response_file in response_files:
-    print(response_file)
+def get_dataframe(response_file):
     data = read_into_dataframe(response_file)
     data = data.applymap(lambda x: np.nan if x == "None" else x)
-    print(data)
-    
+    return data
+
+
+# In[54]:
+
+def get_cols_of_interest(df):
     col_of_interest = ['estm_orig', 'participant_response', 'condition_number', 'bp_d', 'bp_s', 'pulse']
-    data_of_interest = data[col_of_interest]
-    # print(data_of_interest.head(20))
-    data_of_interest['abs_error'] = abs(data_of_interest['estm_orig'] - data_of_interest['participant_response'])
+    assert isinstance(col_of_interest, list)
+    data_of_interest = df[col_of_interest]
+    return data_of_interest
 
+
+# In[55]:
+
+def clean_dataframe(df):
+    df['abs_error'] = abs(df['estm_orig'].astype(float) - df['participant_response'].astype(float))
     
-    data_with_group = pd.concat([data_of_interest, generate_sections()], axis=1)
+    data_with_group = pd.concat([df, generate_sections()], axis=1)
 
-    avg_abs_error = pd.DataFrame(data_with_group.groupby('section_number').agg({
-        'abs_error':np.mean
-        }))
+    # get average error per section
+    avg_abs_error = pd.DataFrame(data_with_group.groupby('section_number').agg({'abs_error':np.mean}))
+    # avg_abs_error = pd.DataFrame(data_with_group.groupby('section_number').mean())
 
+    # aet condition number, bp, pulse and section number
     data_drop_na = data_with_group
     data_drop_na = data_drop_na.dropna()[['condition_number', 'bp_d', 'bp_s', 'pulse', 'section_number']]
 
+    # concatenate the average time by ending time results
     avg_abs_error.index.name = 'section_number'
     avg_abs_error.reset_index(inplace=True)
+    
     data_merged = pd.merge(avg_abs_error, data_drop_na, on='section_number')
+    return data_merged
 
-    colnames = data_merged.columns
-    df = pd.DataFrame([[response_files[0].split('.')[0], data_merged.at[0, 'condition_number']]],
-                      columns=['participant', 'condition'])
-    for line in data_merged.iterrows():
-        line_data = {}
-        # print(line)
-        section_number = str(int(line[1][0]))
-        # print('section number: ', section_number)
 
-        abs_error_key = 'abs_error_' + section_number
-        abs_error_value = line[1][1]
-        # print('abs_error_key: ', abs_error_key,' ; abs_error_value: ', abs_error_value)
+# In[56]:
 
-        bp_d_key = 'bp_d_' + section_number
-        bp_d_value = line[1][3]
-        # print('bp_d_key: ', bp_d_key,' ; bp_d_value: ', bp_d_value)
+def get_totals(response_file):
+    df = get_dataframe(response_file)
+    df = get_cols_of_interest(df)
+    df['abs_error'] = abs(df['estm_orig'].astype(float) - df['participant_response'].astype(float))
+    sum_abs_error = df['abs_error'].astype(float).sum(axis=2)
+    sum_obs = df['participant_response'].count()
+    return sum_abs_error, sum_obs
 
-        bp_s_key = 'bp_s_' + section_number
-        bp_s_value = line[1][4]
-        # print('bp_s_key: ', bp_s_key,' ; bp_s_value: ', bp_s_value)
 
-        pulse_key = 'pulse_' + section_number
-        pulse_value = line[1][5]
-        # print('pulse_key: ', pulse_key,' ; pulse_value: ', pulse_value)
+# In[57]:
 
-        row_df = pd.DataFrame([[abs_error_value, bp_d_value, bp_s_value, pulse_value]],
-                              columns=[abs_error_key, bp_d_key, bp_s_key, pulse_key])
-        df = pd.concat([df, row_df],axis=1)
+np.mean([1, 3, 5, np.nan])
+
+
+# In[58]:
+
+df_all = pd.DataFrame()
+for response_file in response_files:
+    df = get_dataframe(response_file)
+    df = get_cols_of_interest(df)
+    df = clean_dataframe(df)
+    df['respondent'] = response_file
+    total_abs_error, total_abs_obs = get_totals(response_file)
+    df['total_abs_error'], df['total_abs_obs'] = total_abs_error, total_abs_obs
     df_all = df_all.append(df)
 df_all
 
 
+# In[76]:
+
+final = df_all.pivot('respondent', 'section_number')
+final
+
+
+# In[78]:
+
+final.to_csv("bp_pulse.csv")
+
+
 # In[ ]:
 
-df_all.to_csv("bp_pulse.csv")
 
-
-# In[ ]:
-
-# this is a test block before loop
-print(response_files[1])
-data = read_into_dataframe(response_files[1])
-
-col_of_interest = ['estm_orig', 'participant_response', 'condition_number', 'bp_d', 'bp_s', 'pulse']
-data_of_interest = data[col_of_interest]
-data_of_interest['abs_error'] = abs(data_of_interest['estm_orig'] - data_of_interest['participant_response'])
-
-data_with_group = pd.concat([data_of_interest, generate_sections()], axis=1)
-
-avg_abs_error = pd.DataFrame(data_with_group.groupby('section_number').agg({
-    'abs_error':np.mean
-    }))
-
-data_drop_na = data_with_group
-data_drop_na = data_drop_na.dropna()[['condition_number', 'bp_d', 'bp_s', 'pulse', 'section_number']]
-
-avg_abs_error.index.name = 'section_number'
-avg_abs_error.reset_index(inplace=True)
-data_merged = pd.merge(avg_abs_error, data_drop_na, on='section_number')
-
-colnames = data_merged.columns
-df = pd.DataFrame([[response_files[0].split('.')[0], data_merged.at[0, 'condition_number']]],
-                  columns=['participant', 'condition'])
-
-for line in data_merged.iterrows():
-    line_data = {}
-    # print(line)
-    section_number = str(int(line[1][0]))
-    # print('section number: ', section_number)
-    
-    abs_error_key = 'abs_error_' + section_number
-    abs_error_value = line[1][1]
-    # print('abs_error_key: ', abs_error_key,' ; abs_error_value: ', abs_error_value)
-    
-    bp_d_key = 'bp_d_' + section_number
-    bp_d_value = line[1][3]
-    # print('bp_d_key: ', bp_d_key,' ; bp_d_value: ', bp_d_value)
-    
-    bp_s_key = 'bp_s_' + section_number
-    bp_s_value = line[1][4]
-    # print('bp_s_key: ', bp_s_key,' ; bp_s_value: ', bp_s_value)
-    
-    pulse_key = 'pulse_' + section_number
-    pulse_value = line[1][5]
-    # print('pulse_key: ', pulse_key,' ; pulse_value: ', pulse_value)
-    
-    row_df = pd.DataFrame([[abs_error_value, bp_d_value, bp_s_value, pulse_value]],
-                          columns=[abs_error_key, bp_d_key, bp_s_key, pulse_key])
-    df = pd.concat([df, row_df],axis=1)
-df
-#data_merged.pivot('section_number', 'condition_number', 'abs_error')
 