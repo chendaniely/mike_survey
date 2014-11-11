c3fe0a2f95960489a12d1b97350d437c7df453ae
Daniel Chen <Maveri2k@yahoo.com>
2014-11-10 22:27:58 -0500

    replace the \ in respondent answers, this was causing error

--- long_err_bp_pulse.py
+++ long_err_bp_pulse.py
@@ -1,20 +1,21 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[65]:
 
 import pandas as pd
 import numpy as np
 import glob
+import re
 
 
-# In[2]:
+# In[66]:
 
 response_files = glob.glob("*.txt")
 response_files
 
 
-# In[3]:
+# In[67]:
 
 def data_set_column_names():
     colnames = ['time', 'orgi_trial', 'adv', 'mksh', 'revg', 'price_d', 'price_r', 'estm_orig', 'time_r', 
@@ -24,13 +25,13 @@ def data_set_column_names():
     return colnames
 
 
-# In[4]:
+# In[68]:
 
 def read_into_dataframe(response_file):
     return pd.read_csv(response_file, names = data_set_column_names())
 
 
-# In[5]:
+# In[69]:
 
 def generate_sections():
     my_list = []
@@ -41,7 +42,7 @@ def generate_sections():
     return pd.DataFrame(my_list, columns=['section_number'])
 
 
-# In[6]:
+# In[70]:
 
 def get_dataframe(response_file):
     data = read_into_dataframe(response_file)
@@ -49,7 +50,7 @@ def get_dataframe(response_file):
     return data
 
 
-# In[7]:
+# In[71]:
 
 def get_cols_of_interest(df):
     col_of_interest = ['price_r', 'participant_response', 'condition_number', 'bp_d', 'bp_s', 'pulse']
@@ -58,9 +59,25 @@ def get_cols_of_interest(df):
     return data_of_interest
 
 
-# In[8]:
+# In[72]:
+
+def strip_non_numeric_characters(vector):
+    non_decimal = re.compile(r'[^\d.]+')
+    print(vector)
+    cleaned_list = list(non_decimal.sub('', x) for x in vector)
+    cleaned_vector = np.array(cleaned_list)
+    print(cleaned_vector)
+    return cleaned_vector
+
+
+# In[102]:
 
 def clean_dataframe(df):
+    print(df.head())
+    # df['price_r'] = df['price_r'].str.replace('\\', '')                                              
+    df['participant_response'] = df['participant_response'].map(lambda x: str(x).replace('\\', ''))
+    # df['participant_response'] = df['participant_response'].apply(strip_non_numeric_characters)
+    
     df['abs_error'] = abs(df['price_r'].astype(float) - df['participant_response'].astype(float))
     
     data_with_group = pd.concat([df, generate_sections()], axis=1)
@@ -81,21 +98,25 @@ def clean_dataframe(df):
     return data_merged
 
 
-# In[9]:
+# In[103]:
 
 def get_totals(response_file):
     df = get_dataframe(response_file)
     df = get_cols_of_interest(df)
+    # following line of code is copied from the clean_dataframe function
+    df['participant_response'] = df['participant_response'].map(lambda x: str(x).replace('\\', ''))
+    
     df['abs_error'] = abs(df['price_r'].astype(float) - df['participant_response'].astype(float))
     sum_abs_error = df['abs_error'].astype(float).sum(axis=2)
     sum_obs = df['participant_response'].count()
     return sum_abs_error, sum_obs
 
 
-# In[10]:
+# In[104]:
 
 df_all = pd.DataFrame()
 for response_file in response_files:
+    print(response_file, ' start')
     df = get_dataframe(response_file)
     df = get_cols_of_interest(df)
     df = clean_dataframe(df)
@@ -103,16 +124,22 @@ for response_file in response_files:
     total_abs_error, total_abs_obs = get_totals(response_file)
     df['total_abs_error'], df['total_abs_obs'] = total_abs_error, total_abs_obs
     df_all = df_all.append(df)
+    print(response_file, ' finish')
 df_all
 
 
-# In[11]:
+# In[105]:
 
 final = df_all.pivot('respondent', 'section_number')
 final
 
 
-# In[12]:
+# In[106]:
 
 final.to_csv("bp_pulse.csv")
+
+
+# In[ ]:
+
+
 
